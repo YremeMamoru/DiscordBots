e36864ae33f08b7118643988f39d882e1597f462
YremeMamoru <37820903+YremeMamoru@users.noreply.github.com>
2019-05-27 19:47:36 -0300

    Update disc.py

--- DiscordBot/disc.py
+++ DiscordBot/disc.py
@@ -1,21 +1,50 @@
 import disLog
 import discord
+from discord.ext import commands
 
 disLog.logActions()
 
-TOKEN = 'ur shit'
+TOKEN = 'Your token here'
+'''
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('------------')
 
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith(';'): #== ';who':
+            await message.channel.send("Eu sou o super mad scientist maluco, HOUOIN KYOUMA DA!")
+
+
+
+'''
 client = discord.Client()
+'''
+@bot.command()
+async def square(number):
+    squared_value = int(number) * int(number)
+    await square.send(str(number) + " squared is " + str(squared_value))
+'''
 
 @client.event
 async def on_message(message):
     channel = message.channel
+    msg = message.content
     if message.author == client.user:
         return
 
-    if message.content == ';hello':
-        msg = 'Hello {0.author.mention}'.format(message)
-        await channel.send(msg)
+    if msg.startswith(';square '):
+        splitMsg = msg.split()
+        number = float(splitMsg[1])
+        squared = int(number*number)
+
+        await channel.send(str(number) + " ao quadrado é " + str(squared))
+
 
 @client.event
 async def on_ready():
@@ -24,4 +53,7 @@ async def on_ready():
     print(client.user.id)
     print('------------')
 
+
+
+#client = MyClient()
 client.run(TOKEN)
